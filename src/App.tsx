6776d403f16b14aad08cfba6aabf66de82f8e56f
majifo99 <135087859+majifo99@users.noreply.github.com>
2024-03-23 13:48:06 -0600

    prueba

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import Content from './quiz02/Content'
 
 function App() {
 
-  const [products] = [
+  const [product] = [
     { id:1, step:1, name: 'Mouse', price: 60 },
     { id:2, step:1, name: 'Monitor', price: 70 },
     { id:4, step:2, name: 'Keyboard', price: 80 },
